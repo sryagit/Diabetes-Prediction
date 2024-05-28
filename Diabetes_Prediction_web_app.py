db24a2cdd97448d2b159caee0ad8381465320503
sryagit <158165193+sryagit@users.noreply.github.com>
2024-05-28 21:12:23 +0530

    Update Diabetes_Prediction_web_app.py

--- Diabetes_Prediction_web_app.py
+++ Diabetes_Prediction_web_app.py
@@ -1,23 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 18 19:15:01 2021
-
-@author: siddhardhan
-"""
 
 import numpy as np
 import pickle
 import streamlit as st
 
-
 # loading the saved model
 loaded_model = pickle.load(open('trained_model.sav', 'rb'))
-
 
 # creating a function for Prediction
 
 def diabetes_prediction(input_data):
-    
 
     # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
@@ -33,17 +24,12 @@ def diabetes_prediction(input_data):
     else:
       return 'The person is diabetic'
   
-    
-  
 def main():
-    
     
     # giving a title
     st.title('Diabetes Prediction Web App')
     
-    
     # getting the input data from the user
-    
     
     Pregnancies = st.text_input('Number of Pregnancies')
     Glucose = st.text_input('Glucose Level')
@@ -54,7 +40,6 @@ def main():
     DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
     Age = st.text_input('Age of the Person')
     
-    
     # code for Prediction
     diagnosis = ''
     
@@ -62,25 +47,11 @@ def main():
     
     if st.button('Diabetes Test Result'):
         diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-        
-        
+           
     st.success(diagnosis)
-    
-    
-    
-    
     
 if __name__ == '__main__':
     main()
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
